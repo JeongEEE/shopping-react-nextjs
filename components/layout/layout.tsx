ee9a5d234926740734775bebc3de46204909bacb
Gyu Jeong Kim <gjkim@beyless.com>
2023-03-24 14:35:41 +0900

    Emotion Dynamic css props

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -4,9 +4,11 @@ import { css, jsx } from '@emotion/react'
 import { useRouter } from 'next/router'
 import { auth } from '../../firebaseConfig'
 
-const mainContent = css`
-	max-width: 1100px;
-	min-width: 1100px;
+const mainContent = (props) => css`
+	max-width: ${props.wide === 0 ? '1100px' : '1300px'};
+	min-width: ${props.wide === 0 ? '1100px' : '1300px'};
+	/* max-width: 1100px;
+	min-width: 1100px; */
 	margin-left: auto;
 	margin-right: auto;
 	min-height: 100vh;
@@ -15,6 +17,7 @@ const mainContent = css`
 const Layout = (props) => {
 	const router = useRouter()
 	const [noNav, setNoNav] = useState(false);
+	const [wide, setWide] = useState(0);
 
 	useEffect(() => {
 		// firebase 로그인 상태 변경을 감지
@@ -26,14 +29,19 @@ const Layout = (props) => {
       }
     });
 
+		if(router.asPath.includes('/search')) setWide(1);
+		router.events.on('routeChangeComplete', (url) => {
+			if(url.includes('/search')) setWide(1);
+			else setWide(0);
+    })
 		return () => {
-			
+			router.events.off('routeChangeComplete', () => { })
 		}
 	}, [])
 
   return (
     <Fragment>
-			<div css={mainContent}>
+			<div css={mainContent({wide})}>
 				<MainNavigation />
 				<main>{props.children}</main>
 			</div>
--- pages/search/[category].tsx
+++ pages/search/[category].tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
 export async function getServerSideProps({ query: { category } }) {
 	// 새로고침할때 쿼리값이 날라가는걸 방지하기위해 서버사이드로 쿼리를 받아옴
